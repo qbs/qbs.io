64c56635623bfd5d9357486f6230a3cb5741d307
Jan Blackquill <uhhadd@gmail.com>
2021-04-03 19:24:25 -0400

    init | initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: ['themes/qbs/layouts/**/*.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'gray': {
+          DEFAULT: '#5E5E5E',
+          '50': '#D1D1D1',
+          '100': '#C4C4C4',
+          '200': '#ABABAB',
+          '300': '#919191',
+          '400': '#787878',
+          '500': '#5E5E5E',
+          '600': '#454545',
+          '700': '#2B2B2B',
+          '800': '#121212',
+          '900': '#000000'
+        },
+      },
+    },
+    fontFamily: {
+      'display': ['Inter', 'system-ui'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
