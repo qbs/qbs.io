2a9c46679e85b9c70dda8db59f03a7f6838dc728
Jan Blackquill <uhhadd@gmail.com>
2021-06-12 17:40:51 -0400

    feat | improved docs look

--- scripts/_generate.sh
+++ scripts/_generate.sh
@@ -2,14 +2,16 @@
 
 shopt -s nullglob
 
+export QT_INSTALL_DOCS=/home/jpontaoski/kde/qt5/doc
+export QBS_VERSION=master
+export QBS_VERSION_TAG=master
+
 pushd _qbs/doc || exit 1
 qdoc qbs-hugo.qdocconf --outputdir=../../../content/docs
 popd || exit 1
 
-for f in ../content/docs/*.html
-do
-    echo "Postproc $f"
-    ./adjust.py "$f" || exit 1
-done
+echo "postprocessing with adjust.mjs..."
+
+./adjust.mjs ../content/docs/*.html || exit 1
 
 mv ../content/docs/index.html ../content/docs/_index.html
---
+++ scripts/adjust.mjs
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import jsdom from "jsdom";
+import fs from "fs";
+import fsPromise from "fs/promises";
+
+const args = process.argv.slice(2)
+
+Promise.all(args.map(async (arg) => {
+    const data = await fsPromise.readFile(arg)
+    const dom = new jsdom.JSDOM(data)
+
+    const elm = dom.window.document.body
+    elm.querySelectorAll("[href]").forEach((it) => {
+        let attr = it.getAttribute("href")
+
+        if (attr == "index.html") {
+            attr = "/docs"
+        } else {
+            attr = "/docs/" + attr.replace(".html", "/")
+        }
+
+        it.setAttribute("href", attr)
+    })
+    elm.querySelectorAll(".qmlproto").forEach((it) => {
+        const readOnly = it.querySelector(".qmlreadonly") !== null
+
+        const name = it.querySelector(".name")
+        let type = it.querySelector(".type a")
+        if (!type) {
+            type = {outerHTML: it.querySelector(".type").textContent}
+        } else {
+            type = type.outerHTML
+        }
+        it.outerHTML = `<h3 class="qmlproto">${readOnly ? `<span class="opacity-50">readonly </span>` : ""}${name.textContent}: ${type.outerHTML}</h3>`
+    })
+
+    return fsPromise.writeFile(arg, "---\n\n---\n" + elm.innerHTML)
+}))
