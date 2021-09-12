383c5a23532cf619f03980b16e95e8873e34c693
Ivan Komissarov <ABBAPOH@gmail.com>
2021-09-12 06:58:33 +0300

    build | don't hardcode QDoc vars

--- scripts/_generate.sh
+++ scripts/_generate.sh
@@ -2,9 +2,21 @@
 
 shopt -s nullglob
 
-export QT_INSTALL_DOCS=/home/jpontaoski/kde/qt5/doc
-export QBS_VERSION=master
-export QBS_VERSION_TAG=master
+export QMAKE_PATH=${QMAKE_PATH:-$(which qmake)}
+
+export QT_INSTALL_DOCS=${QT_INSTALL_DOCS:-}
+
+if [ -z "${QT_INSTALL_DOCS}" ]; then
+    if [ -z "${QMAKE_PATH}" ]; then
+        echo "Either QT_INSTALL_DOCS must be set or qmake should be in PATH"
+        exit 1
+    else
+        export QT_INSTALL_DOCS=$(dirname ${QMAKE_PATH})/../doc
+    fi
+fi
+
+export QBS_VERSION=$(cat _qbs/VERSION)
+export QBS_VERSION_TAG=${QBS_VERSION//[.-]/}
 
 pushd _qbs/doc || exit 1
 qdoc qbs-hugo.qdocconf --outputdir=../../../content/docs
